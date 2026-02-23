3c96b31d50564ec9c89c8a0d85d22e044241276b
hwtransport <martj20000t@gmail.com>
2026-02-23 15:25:53 +0900

    파이어베이스 실시간 연동 완료

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; 
 import { getMessaging } from "firebase/messaging";
 
-// ⚠️ 여기에 방금 복사하신 본인의 firebaseConfig 코드를 덮어쓰세요!
+// 기사님이 직접 확인해주신 현우운수 전용 키값입니다.
 const firebaseConfig = {
   apiKey: "AIzaSyD1An_fN5nk0ZpfANTL_6h1zzKXYa6OiPs",
   authDomain: "hwt-app-fcd56.firebaseapp.com",
@@ -11,8 +12,6 @@ const firebaseConfig = {
   appId: "1:697712630635:web:ee0edaeff5d71e72644a2e"
 };
 
-// 파이어베이스 초기화 (앱 시작)
 const app = initializeApp(firebaseConfig);
-
-// 푸시 알림 기능 내보내기
-export const messaging = getMessaging(app);
+export const db = getFirestore(app);
+export const messaging = typeof window !== "undefined" ? getMessaging(app) : null;
